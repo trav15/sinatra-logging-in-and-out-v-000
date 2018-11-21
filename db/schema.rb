c6625b9b9730e09cffc0e54c16876436cd1a673b
Travis T <travistokuyama@gmail.com>
2018-11-21 20:23:56 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,10 @@
 
 ActiveRecord::Schema.define(version: 20181121195045) do
 
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.decimal "balance"
+  end
+
 end
